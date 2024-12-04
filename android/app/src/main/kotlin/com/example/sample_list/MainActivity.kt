03349351d1851b7e9bd1f3374cc0fbff72d3077a
Jitesh Sharma <jitesh.sharma@htdigital.in>
2024-12-04 16:46:11 +0530

    commit... initial commmit

---
+++ android/app/src/main/kotlin/com/example/sample_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sample_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/model.dart
@@ -0,0 +1,11 @@
+class NewsItem {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  NewsItem({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+}
