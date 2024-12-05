67069efa472e4bbf4eccd56bbb471c7622c33e56
Jitesh Sharma <jitesh.sharma@htdigital.in>
2024-12-05 13:51:52 +0530

    Remove all ignored files from Git tracking

--- .dart_tool/flutter_build/7ad727058af5d0a56b1d1464d55b5fd9/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/d111520/Documents/iOS\ WorkSpace/flutter-Demo/.dart_tool/flutter_build/7ad727058af5d0a56b1d1464d55b5fd9/native_assets.yaml: 
--- .dart_tool/flutter_build/e3c510844c10c6e407856b9392f2b279/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/d111520/Documents/iOS\ WorkSpace/flutter-Demo/.dart_tool/flutter_build/e3c510844c10c6e407856b9392f2b279/native_assets.yaml: 
